328786b73566a0e385dd50220b147dfc6ffcd5c3
Broctholumeu <Brocksqd@gmail.com>
2022-03-25 18:01:19 -0500

    Clear clutter in app

--- salty-sous-chef/src/App.js
+++ salty-sous-chef/src/App.js
@@ -5,20 +5,7 @@ import DropDown from './DropDown';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <DropDown />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <DropDown />
     </div>
   );
 }
