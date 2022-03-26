110efbb4e55acd44d669e73dfbc157f0be06bba8
Broctholumeu <Brocksqd@gmail.com>
2022-03-25 19:09:18 -0500

    Styling and Format

--- salty-sous-chef/src/App.js
+++ salty-sous-chef/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import DropDown from './DropDown.js';
 import ButtonAction from './Button.js';
+import TextOutput from './TextOutput';
 import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
+      <TextOutput />
       <Stack direction="row" spacing={2}>
         <DropDown /> 
         <ButtonAction />
---
+++ salty-sous-chef/src/TextOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+
+export default function TextOutput() {
+    return(
+        <p>Hello World</p>
+    );
+}
